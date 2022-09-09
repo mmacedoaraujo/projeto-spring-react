23bd2209c0c0eef49413bda37b932f98bcede275
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-09-09 12:25:24 -0300

    repository created

---
+++ backend/src/main/java/com/mmacedoaraujo/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.mmacedoaraujo.dsmeta.repositories;
+
+import com.mmacedoaraujo.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
